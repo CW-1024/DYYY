0f3b2c015b6cd94fce6be201d9e76d1ab4a5853a
陈某人 <852120552@qq.com>
2025-04-20 03:31:12 +0800

    更新 AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -120,8 +120,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @interface AWELongPressPanelManager : NSObject
 + (instancetype)shareInstance;
 - (void)dismissWithAnimation:(BOOL)animated completion:(void (^)(void))completion;
-- (BOOL)shouldShowMordenLongPressPanel;
-- (BOOL)showShareFriends;
 @end
 
 @interface AWENormalModeTabBarGeneralButton : UIButton
@@ -164,7 +162,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 - (void)performCommentAction;
 - (void)performLikeAction;
 - (void)showSharePanel;
-- (void)showDislikeOnVideo;
 - (void)onVideoPlayerViewDoubleClicked:(id)arg1;
 - (UIViewController *)firstAvailableUIViewController;
 - (void)speedButtonTapped:(id)sender;
@@ -649,14 +646,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @interface AWELiveFlowAlertView : UIView
 @end
 
-//搜索视频底部评论视图
-@interface AWECommentInputBackgroundView : UIView
-@end
-
-//聊天视频底部快速回复视图
-@interface AWEIMFeedBottomQuickEmojiInputBar : UIView
-@end
-
 @interface DUXBadge : UIView
 @end
 
@@ -673,14 +662,6 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @end
 
 @interface AWEHPTopBarCTAItemView : UIView
-@end
-
-//应用内推送容器
-@interface AWEInnerNotificationWindow : UIWindow
-- (void)setupBlurEffectForNotificationView;
-- (void)applyBlurEffectToView:(UIView *)containerView;
-- (void)setLabelsColorWhiteInView:(UIView *)view;
-- (void)clearBackgroundRecursivelyInView:(UIView *)view;
 @end
 
 @interface AWEFakeProgressSliderView : UIView
